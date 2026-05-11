4531f4d7165d2be417b68b3f1b972e370711c1ea
youamar <youamar@users.noreply.github.com>
2026-05-11 18:20:37 +0800

    Bump SW cache to v3 to force-update PWA clients

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-journey-v2';
+const CACHE = 'our-journey-v3';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
