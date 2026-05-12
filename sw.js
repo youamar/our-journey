0587724486b614031f03a2ce638d0db55bf6653a
youamar <youamar@users.noreply.github.com>
2026-05-12 13:23:37 +0800

    Clean up dead FAB CSS rules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-journey-v12';
+const CACHE = 'our-journey-v13';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
