aa39a432972f37b25d56db82cfed9d08f929e90c
youamar <youamar@users.noreply.github.com>
2026-05-12 13:15:47 +0800

    Move Gallery nav link between 05 and 06 to match section order

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-journey-v9';
+const CACHE = 'our-journey-v10';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
